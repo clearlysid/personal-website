05728b592b4d8bb9cc3b5d5aeb5a9cb5f4a9b395
clearlysid <yourstrulysiddharth@gmail.com>
2021-01-07 21:38:19 +0530

    ✒️ font optimizations and copy updates

---
+++ _data/env.js
@@ -0,0 +1,12 @@
+const environment = process.env.NODE_ENV;
+const PROD_ENV = "production";
+const prodUrl = "https://www.sidds.me";
+const devUrl = "http://localhost:8080";
+const baseUrl = environment === PROD_ENV ? prodUrl : devUrl;
+const isProduction = environment === PROD_ENV;
+
+module.exports = {
+	environment,
+	isProduction,
+	baseUrl,
+};
