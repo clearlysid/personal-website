99397032500fef572d922455fdc1f9352ec19481
clearlysid <yourstrulysiddharth@gmail.com>
2021-02-05 23:45:11 +0530

    🎨 updated pages to latest design

--- scripts/main.js
+++ scripts/main.js
@@ -13,18 +13,6 @@ function init() {
 			});
 		}
 	}
-
-	if (document.querySelector(".project-list") && window.innerWidth > 700) {
-		document.querySelectorAll(".project-item").forEach((p, i) => {
-			new ProjectHover(p, i);
-		});
-
-		const homeTitle = document.querySelector(".home-grid > h1");
-
-		homeTitle.innerHTML = Array.from(homeTitle.innerText)
-			.map((l) => `<span>${l}</span>`)
-			.join("");
-	}
 }
 
 init();
