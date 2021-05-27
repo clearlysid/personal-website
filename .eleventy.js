4bc811cc62fc2ce48389a698089451a2c00889e6
Siddharth Jha <yourstrulysiddharth@gmail.com>
2021-05-28 03:34:52 +0530

    🚨 rolled back to basic theme

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,8 @@
-const NotionRenderer = require("./_includes/notion");
-const CloudinaryRenderer = require("./_includes/cloudinary");
+const RenderNotion = require("./_includes/notion");
+const Cloudinary = require("./_includes/cloudinary");
 
 module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy({ assets: "assets" });
-	eleventyConfig.addShortcode("notion", (blocks) => NotionRenderer(blocks));
-	eleventyConfig.addShortcode("cloudimage", (url, alt) =>
-		CloudinaryRenderer(url, alt)
-	);
-
-	return {
-		dir: {
-			output: "public",
-		},
-	};
+	eleventyConfig.addShortcode("notion", (blocks) => RenderNotion(blocks));
+	eleventyConfig.addShortcode("cloudimage", (u, a) => Cloudinary(u, a));
 };
