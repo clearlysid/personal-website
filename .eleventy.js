80567cdedeb376f68dac5a523615b0c82b9930aa
Siddharth Jha <yourstrulysiddharth@gmail.com>
2021-05-28 02:57:57 +0530

    🧹 cleanup

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,8 @@
 const NotionRenderer = require("./_includes/notion");
-const CloudinaryRenderer = require("./_includes/cloudinary");
+const Cloudinary = require("./_includes/cloudinary");
 
 module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy({ assets: "assets" });
 	eleventyConfig.addShortcode("notion", (blocks) => NotionRenderer(blocks));
-	eleventyConfig.addShortcode("cloudimage", (url, alt) =>
-		CloudinaryRenderer(url, alt)
-	);
+	eleventyConfig.addShortcode("cloudimage", (u, a) => Cloudinary(u, a));
 };
