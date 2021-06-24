34eef21514776efbdcb316cd4e9453e79b2b9831
Siddharth Jha <yourstrulysiddharth@gmail.com>
2021-06-25 02:58:42 +0530

    💅 more minimal styling

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,12 @@
-function init() {
-	// A demo: add an element to the document, then announce it
-	const alertNode = document.createElement("div");
-	const mainNode = document.querySelector("body");
-
-	alertNode.setAttribute("role", "status");
-	alertNode.setAttribute("aria-live", "polite");
-	mainNode.appendChild(alertNode);
-
-	// Wait some arbitrary time, then populate it
+function TestVite() {
 	setTimeout(() => {
-		const successNode = document.createElement("p");
-		// Let's verify that Vite is injecting environment variables
-		// @see https://vitejs.dev/guide/env-and-mode.html#env-variables
 		if (import.meta.env.DEV === true) {
-			successNode.innerText = "Vite is serving the script correctly!";
+			console.log("vite is working!");
 		}
 		if (import.meta.env.PROD === true) {
-			successNode.innerText =
-				"Vite has built the files statically and Eleventy injected the correct script tag!";
+			console.log("vite built, and 11ty injected correct script tag");
 		}
-
-		alertNode.appendChild(successNode);
 	}, 400);
 }
 
-init();
+TestVite();
