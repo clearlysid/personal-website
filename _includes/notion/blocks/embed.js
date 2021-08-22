3fe1bfe1c2f782d2f7b55e69b99263bd5ec8359e
Siddharth Jha <siddharth99c@gmail.com>
2021-08-22 19:43:51 +0530

    🐛 fixed compiler issue

--- _includes/notion/blocks/embed.js
+++ _includes/notion/blocks/embed.js
@@ -1,11 +1,7 @@
-module.exports = function RenderEmbed(blockValue, children) {
+module.exports = function RenderEmbed(value) {
 	switch (value.type) {
 		case "figma":
 			return `<iframe class="notion" src="${value.properties.source[0][0]
-				}" style="height:500px;"></iframe>
-						${value.properties.caption &&
-				`<figcaption class="notion-image-caption">${renderText(
-					value.properties.caption
-				)}</figcaption>`
-				}`;
-	};
+				}" style="height:500px;"></iframe>}`;
+	}
+}
