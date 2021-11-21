2b63a64ce123ac28a0ed59cfa3d849e8f6b2699d
clearlysid <siddharth99c@gmail.com>
2021-11-22 01:38:37 +0530

    💥 removes redundant files

--- _data/global.js
+++ _data/global.js
@@ -2,7 +2,7 @@ module.exports = {
 	env: process.env.NODE_ENV,
 	title: "Siddharth Jha — UX and Interaction Designer",
 	description: "Building stuff for the internet and having a blast doing it!",
-	image: "https://www.siddharthjha.com/assets/profile.jpg",
+	image: "",
 	url: "https://www.siddharthjha.com",
 	author: "Siddharth Jha",
 	username: "@clearlysid",
