7716b49c14a088e122743b20d912d970857a81b4
clearlysid <siddharth99c@gmail.com>
2021-11-21 21:27:38 +0530

    💣 refactor: data files

---
+++ _data/global.js
@@ -0,0 +1,9 @@
+module.exports = {
+	env: process.env.NODE_ENV,
+	title: "Siddharth Jha — UX and Interaction Designer",
+	description: "Building stuff for the internet and having a blast doing it!",
+	image: "https://www.siddharthjha.com/assets/profile.jpg",
+	url: "https://www.siddharthjha.com",
+	author: "Siddharth Jha",
+	username: "@clearlysid",
+}
