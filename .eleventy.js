9c889fd7ebe80ac66ac864a9854b34b75ff59d72
clearlysid <siddharth99c@gmail.com>
2021-11-24 23:15:07 +0530

    🙈 ignore notes coming from dropbox

--- .eleventy.js
+++ .eleventy.js
@@ -1,49 +1,45 @@
-const fs = require("fs").promises;
-const path = require("path");
+const path = require("path")
+const fs = require("fs").promises
 
 module.exports = function (eleventyConfig) {
 	eleventyConfig.setQuietMode(true)
 	eleventyConfig.addPassthroughCopy({ assets: "assets" })
-	eleventyConfig.addCollection("notes", (c) => c.getFilteredByGlob("notes/*.md"));
+	eleventyConfig.addCollection("notes", (c) => c.getFilteredByGlob("notes/*.md"))
+	eleventyConfig.setUseGitIgnore(false)
 
 	// Read Vite's manifest.json, add script tags for entry files
 	// you could probably read vite.config.js and get that information directly
 	// @see https://vitejs.dev/guide/backend-integration.html
 
 	const getChunkInformationFor = async (entryFile) => {
-		// We want an entryFile, because in practice you might have multiple entrypoints
-		if (!entryFile) throw new Error("Specify an entryFile to viteScript.");
+		if (!entryFile) throw new Error("Specify an entry to vite")
 
-		// TODO: Consider caching this call, to avoid going to the filesystem every time
-		const manifest = await fs.readFile(
-			path.resolve(process.cwd(), "_site", "manifest.json")
-		);
+		const manifest = await fs.readFile(path.resolve(process.cwd(), "_site", "manifest.json"))
+		const parsed = JSON.parse(manifest)
+		let entryChunk = parsed[entryFile]
 
-		const parsed = JSON.parse(manifest);
-		let entryChunk = parsed[entryFilename];
+		if (!entryChunk) throw new Error(`${entryFile} not found in the manifest.`)
 
-		if (!entryChunk) throw new Error(`${entryFilename} not found in the manifest.`);
-
-		return entryChunk;
+		return entryChunk
 	}
 
 	const viteScript = async (entryFile) => {
-		const entryChunk = await getChunkInformationFor(entryFile);
-		return `<script type="module" src="/${entryChunk.file}"></script>`;
-	};
+		const entryChunk = await getChunkInformationFor(entryFile)
+		return `<script type="module" src="/${entryChunk.file}"></script>`
+	}
 
 	const viteStyles = async (entryFile) => {
-		const entryChunk = await getChunkInformationFor(entryFile);
+		const entryChunk = await getChunkInformationFor(entryFile)
 		if (!entryChunk.css || entryChunk.css.length === 0) {
-			console.warn(`No css found for ${entryFile} entry`);
-			return "";
+			console.warn(`No css found for ${entryFile} entry`)
+			return ""
 		}
 		// Assume multiple CSS files per entry by default
 		return entryChunk.css
 			.map((css) => `<link rel="stylesheet" href="/${css}"></link>`)
-			.join("\n");
+			.join("\n")
 	}
 
-	eleventyConfig.addShortcode("viteScript", viteScript);
-	eleventyConfig.addShortcode("viteStyles", viteStyles);
-};
+	eleventyConfig.addShortcode("viteScript", viteScript)
+	eleventyConfig.addShortcode("viteStyles", viteStyles)
+}
