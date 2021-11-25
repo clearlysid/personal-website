9056276095463676ea84a180d6ae176db9f4afa2
clearlysid <siddharth99c@gmail.com>
2021-11-25 21:35:10 +0530

    💅 removes extra styles and adds rough notation

--- notes/kitchen-sink.md
+++ notes/kitchen-sink.md
@@ -6,6 +6,23 @@ published: true
 image: https://images.unsplash.com/photo-1637500970860-90c839da7c39?&auto=format&fit=crop&w=1200&q=80
 ---
 
+This site used to be powered by Notion via a renderer that I wrote, but given how complex the data structures were and the number of block types Notion had — it quickly became hard to maintain. As Notion updated things on their end, my renderer would break until I updated it again.
+
+## Back to Markdown
+
+I moved all my notes back to good ol' `markdown`, but I do miss the flexibility a bit. Unlike Notion, the "types" of blocks here are limited to just:
+
+1. paragraphs
+2. a few headings
+3. lists like this one
+4. quotes
+5. media
+    - images
+    - videos
+6. Oh, and code blocks but that's about it.
+
+> No fancy embeds, no callouts, goodbye nested blocks.
+
 ## Lever Two Heading
 
 Paragraphs and how they look.
--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,40 @@
 import "../styles/main.css";
 import { animate, stagger } from "motion"
+import { annotate } from 'rough-notation';
 
 animate(
 	".blog-container h1 > span",
 	{ y: [20, 0], opacity: [0, 1] },
 	{ duration: 0.3, delay: stagger(0.02) }
 )
+
+const colors = ["#59EFEF", "#4DF8D9", "#FF9AB2", "#CE9BF5"]
+let copy = colors.slice(0);
+
+function randomColour() {
+	if (copy.length < 1) { copy = colors.slice(0); }
+	let index = Math.floor(Math.random() * copy.length);
+	let item = copy[index];
+	copy.splice(index, 1);
+	return item;
+}
+
+document.querySelectorAll('.blog-item-title').forEach(blog => {
+	const blogAnnotation = annotate(blog, {
+		type: 'highlight',
+		animationDuration: 600,
+		color: randomColour(),
+		iterations: 2
+	})
+
+	blog.addEventListener('mouseenter', () => blogAnnotation.show())
+	blog.addEventListener('mouseleave', () => blogAnnotation.hide())
+})
+
+
+document.querySelectorAll('p > a').forEach(link => {
+	const linkAnn = annotate(link, { type: 'circle', iterations: 2, color: randomColour(), strokeWidth: 2 })
+
+	link.addEventListener('mouseenter', () => linkAnn.show())
+	link.addEventListener('mouseleave', () => linkAnn.hide())
+})
