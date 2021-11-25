bcba5109b96927e8573c1d97ec19e425595dc6e2
clearlysid <siddharth99c@gmail.com>
2021-11-25 21:27:33 +0530

    📷 cloudinary image features

--- _includes/cloudinary.js
+++ _includes/cloudinary.js
@@ -1,9 +1,15 @@
-module.exports = function (url, alt, width = 800) {
+module.exports = function (url, alt, width = 1000, className) {
+
+	const mobileWidth = 600 // controls breakpoint + image width
+	const classN = className ? ` class="${className}"` : ``
 
 	const baseUrl = `https://res.cloudinary.com/clearlysid/image/fetch`
-	const altText = alt ? ` alt="${alt}" ` : ""
+	const link = (w) => baseUrl + `/f_auto/q_80/w_${w}/` + encodeURIComponent(url)
 
-	const link = (w) => baseUrl + `/f_auto/w_${w}/` + encodeURIComponent(url)
+	const src = ` src="${link(width)}"`
+	const altText = alt ? ` alt="${alt}"` : ``
+	const sizes = ` sizes="(min-width: ${mobileWidth}px) ${width}px, ${mobileWidth}px"`
+	const srcset = ` srcset="${link(width)} ${width}w, ${link(mobileWidth)}w"`
 
-	return `<img src="${link(width)}"` + altText + `/>`
+	return `<img` + classN + srcset + sizes + src + altText + ` />`
 }
