a7bc56af7b1440ce0b6e50d6f244abb5055d12b2
clearlysid <siddharth99c@gmail.com>
2021-11-25 15:35:43 +0530

    🕵️ refactor seo tags and cleanup

--- _data/global.js
+++ _data/global.js
@@ -1,9 +1,8 @@
 module.exports = {
 	env: process.env.NODE_ENV,
 	title: "Siddharth Jha — UX and Interaction Designer",
-	description: "Building stuff for the internet and having a blast doing it!",
 	image: "",
-	url: "https://www.siddharthjha.com",
-	author: "Siddharth Jha",
+	baseUrl: "https://www.siddharthjha.com",
+	sitename: "Siddharth's Notebook",
 	username: "@clearlysid",
 }
