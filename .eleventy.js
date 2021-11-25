5a3b0f04626d900ff93a84f50715bd4939df03b9
clearlysid <siddharth99c@gmail.com>
2021-11-26 03:46:58 +0530

    ⏰ adds now page, and fixes styles

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,11 @@ const { viteScript, viteStyles } = require("./_includes/vite")
 module.exports = function (eleventyConfig) {
 	eleventyConfig.setQuietMode(true)
 	eleventyConfig.addPassthroughCopy({ assets: "assets" })
-	eleventyConfig.addCollection("notes", (c) => c.getFilteredByGlob("notes/*.md"))
+	eleventyConfig.addCollection("notes", (c) =>
+		c.getFilteredByGlob("notes/*.md")
+			.sort((a, b) => b.date - a.date)
+			.filter((s) => s.data.slug !== "now")
+	)
 	eleventyConfig.setUseGitIgnore(false)
 
 	eleventyConfig.addShortcode("viteScript", viteScript)
