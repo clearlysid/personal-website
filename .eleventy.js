6e20c532da003d053e5d7b16befc962e504e4a3b
clearlysid <siddharth99c@gmail.com>
2021-11-27 02:01:35 +0530

    ✅ adds support for figure captions and todos

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,23 @@
+const markdownIt = require("markdown-it")
+const mdTaskCheckbox = require("markdown-it-task-checkbox")
+const mdImplicitFigures = require("markdown-it-implicit-figures")
 const cloudinary = require("./_includes/cloudinary")
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight")
 
 const { viteScript, viteStyles } = require("./_includes/vite")
 
 module.exports = function (eleventyConfig) {
-
 	// Options
 	eleventyConfig.setQuietMode(true)
 	eleventyConfig.setUseGitIgnore(false)
 	eleventyConfig.addPassthroughCopy({ assets: "assets" })
+
+	// Markdown Plugins
+	const md = markdownIt({ html: true })
+		.use(mdTaskCheckbox)
+		.use(mdImplicitFigures, { figcaption: true })
+
+	eleventyConfig.setLibrary("md", md)
 
 	// Shortcodes
 	eleventyConfig.addShortcode("viteScript", viteScript)
