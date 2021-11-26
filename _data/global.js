2b1c2a01da6b89d66020a4393ec51e7d039a95f2
clearlysid <siddharth99c@gmail.com>
2021-11-27 03:08:45 +0530

    📷 adds og image and tweaks title structure

--- _data/global.js
+++ _data/global.js
@@ -1,7 +1,7 @@
 module.exports = {
 	env: process.env.NODE_ENV,
 	title: "Siddharth Jha — UX and Interaction Designer",
-	image: "",
+	image: "https://res.cloudinary.com/clearlysid/image/upload/v1637962263/generic-open-graph_smlnfp.png",
 	baseUrl: "https://www.siddharthjha.com",
 	sitename: "Siddharth's Notebook",
 	username: "@clearlysid",
