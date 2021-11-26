ed6300c834ad1bb926def963aeab720e811ffc20
clearlysid <siddharth99c@gmail.com>
2021-11-27 00:57:49 +0530

    🗒 refactored notes collection

--- .eleventy.js
+++ .eleventy.js
@@ -1,17 +1,20 @@
 const cloudinary = require("./_includes/cloudinary")
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight")
+
 const { viteScript, viteStyles } = require("./_includes/vite")
 
 module.exports = function (eleventyConfig) {
-	eleventyConfig.setQuietMode(true)
-	eleventyConfig.addPassthroughCopy({ assets: "assets" })
-	eleventyConfig.addCollection("notes", (c) =>
-		c.getFilteredByGlob("notes/*.md")
-			.sort((a, b) => b.date - a.date)
-			.filter((s) => s.data.slug !== "now")
-	)
-	eleventyConfig.setUseGitIgnore(false)
 
+	// Options
+	eleventyConfig.setQuietMode(true)
+	eleventyConfig.setUseGitIgnore(false)
+	eleventyConfig.addPassthroughCopy({ assets: "assets" })
+
+	// Shortcodes
 	eleventyConfig.addShortcode("viteScript", viteScript)
 	eleventyConfig.addShortcode("viteStyles", viteStyles)
 	eleventyConfig.addShortcode("cloudinary", cloudinary)
+
+	// Plugins
+	eleventyConfig.addPlugin(syntaxHighlight)
 }
