a579ad2f41664a2802b0beda61170567725ea93b
clearlysid <siddharth99c@gmail.com>
2022-03-18 19:19:35 +0530

    🎥 adds featurettes

---
+++ _data/features.js
@@ -0,0 +1,20 @@
+module.exports = [
+	{
+		name: "Framer",
+		logo: "",
+		media: "",
+		link: ""
+	},
+	{
+		name: "HelloMeets",
+		logo: "",
+		media: "",
+		link: "",
+	},
+	{
+		name: "King Sidharth",
+		logo: "",
+		media: "",
+		link: "",
+	},
+]
