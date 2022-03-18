199c2e7ef94f9b9957006b2594f6b566ce298664
clearlysid <siddharth99c@gmail.com>
2022-03-18 22:51:13 +0530

    🎉 updates copy, site titles

--- _data/global.js
+++ _data/global.js
@@ -1,6 +1,6 @@
 module.exports = {
 	env: process.env.NODE_ENV,
-	title: "Siddharth Jha / @clearlysid — Designer and Engineer",
+	title: "Siddharth Jha / @clearlysid",
 	image: "https://res.cloudinary.com/clearlysid/image/upload/v1637962263/generic-open-graph_smlnfp.png",
 	baseUrl: "https://www.siddharthjha.com",
 	sitename: "Siddharth / @clearlysid",
