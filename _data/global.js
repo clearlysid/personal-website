a85ae37375e76733a192eca9cf6ee11fc209c21e
clearlysid <siddharth99c@gmail.com>
2022-03-19 00:57:44 +0530

    🔍 fixes for seo and accessibility

--- _data/global.js
+++ _data/global.js
@@ -2,7 +2,7 @@ module.exports = {
 	env: process.env.NODE_ENV,
 	title: "Siddharth Jha / @clearlysid",
 	image: "https://res.cloudinary.com/clearlysid/image/upload/v1637962263/generic-open-graph_smlnfp.png",
-	baseUrl: "https://www.siddharthjha.com",
+	baseurl: "https://www.siddharthjha.com",
 	sitename: "Siddharth / @clearlysid",
 	username: "@clearlysid",
 }
