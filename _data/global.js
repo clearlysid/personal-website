3979367cba3ef973be4d6464d8604e2af78f555a
Siddharth <siddharth99c@gmail.com>
2022-10-17 00:12:45 +0530

    style: removes _data folder

--- _data/global.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	env: process.env.NODE_ENV,
-	title: "Siddharth Jha / @clearlysid",
-	image: "https://res.cloudinary.com/clearlysid/image/upload/v1647637689/open-graph_uxzmph.jpg",
-	baseurl: "https://www.sid.me",
-	sitename: "Siddharth / @clearlysid",
-	username: "@clearlysid",
-}
